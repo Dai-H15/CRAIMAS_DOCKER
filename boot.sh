973a6568999b4cc7a1d771fa4202d180899c8cad
1F10220082 <s1f102200828@iniad.org>
2024-05-25 23:40:03 +0900

    各種修正と構成

---
+++ boot.sh
@@ -0,0 +1,9 @@
+sudo docker compose up -d
+
+docker compose exec django python manage.py makemigrations main view_sheet task_calendar authUser
+
+docker compose exec django python manage.py migrate
+
+docker compose exec django python manage.py collectstatic --noinput
+
+echo "起動が完了しました"
