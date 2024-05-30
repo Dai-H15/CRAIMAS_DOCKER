139baf552f776b6a4d7e5167ff138be2c4cb6576
1F10220082 <s1f102200828@iniad.org>
2024-05-31 00:58:53 +0900

    起動用バッチを修正

--- boot.sh
+++ boot.sh
@@ -1,8 +1,8 @@
 sudo docker compose up -d
 
-docker compose exec django python manage.py makemigrations main view_sheet task_calendar authUser
+docker compose exec django python manage.py makemigrations main view_sheet task_calendar authUser management
 
-docker compose exec django python manage.py migrate
+docker compose exec django python manage.py migrate main view_sheet task_calendar authUser management
 
 docker compose exec django python manage.py collectstatic --noinput
 
