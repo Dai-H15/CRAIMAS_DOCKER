059416868bdf5e1917e4de063823498c969783d5
1F10220082 <s1f102200828@iniad.org>
2024-06-18 14:56:49 +0900

    起動時にtestを実行するように変更

--- boot.sh
+++ boot.sh
@@ -6,4 +6,10 @@ docker compose exec django python manage.py migrate
 
 docker compose exec django python manage.py collectstatic --noinput
 
+docker compose exec django python manage.py test
+
+if [ $? -eq 1 ]; then
+    echo "!!!Warning!!!: テストにエラーや失敗が含まれています"
+fi
+
 echo "起動が完了しました"
