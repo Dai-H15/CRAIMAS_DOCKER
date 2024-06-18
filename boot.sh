86237ea079c2a3c0d84c2547901d044e4e6d8bf2
1F10220082 <s1f102200828@iniad.org>
2024-06-18 15:00:00 +0900

    エラーがなかった場合にも文章を表示

--- boot.sh
+++ boot.sh
@@ -10,6 +10,9 @@ docker compose exec django python manage.py test
 
 if [ $? -eq 1 ]; then
     echo "!!!Warning!!!: テストにエラーや失敗が含まれています"
+    else
+        echo "Success: テストは正常に完了しました"
+    fi
 fi
 
 echo "起動が完了しました"
