48706b0a564399143109585d3c470045f48a1b1f
1F10220082 <s1f102200828@iniad.org>
2024-06-18 15:03:09 +0900

    そもそもdjangoのテスト自体に出力があるので、いらないかも

--- boot.sh
+++ boot.sh
@@ -10,9 +10,7 @@ docker compose exec django python manage.py test
 
 if [ $? -eq 1 ]; then
     echo "!!!Warning!!!: テストにエラーや失敗が含まれています"
-    else
-        echo "Success: テストは正常に完了しました"
-    fi
+    
 fi
 
 echo "起動が完了しました"
