19d682d30897464bf09653f1f5086b33d2cf6409
1F10220082 <s1f102200828@iniad.org>
2024-08-09 01:43:26 +0900

    新しいアプリケーションへの対応

--- boot.sh
+++ boot.sh
@@ -1,6 +1,6 @@
 sudo docker compose up -d
 
-docker compose exec django python manage.py makemigrations main view_sheet task_calendar authUser management support
+docker compose exec django python manage.py makemigrations main view_sheet task_calendar authUser management support ESmanage
 
 docker compose exec django python manage.py migrate
 
@@ -9,8 +9,7 @@ docker compose exec django python manage.py collectstatic --noinput
 docker compose exec django python manage.py test
 
 if [ $? -eq 1 ]; then
-    echo "!!!Warning!!!: テストにエラーや失敗が含まれています"
-    
+    echo "!!!Warning!!!: Error is detected."
 fi
 
 echo "起動が完了しました"
